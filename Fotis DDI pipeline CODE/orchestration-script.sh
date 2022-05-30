e95d42d70d0e8b374f13e218cd1558e09f862241
fotais <60608113+fotais@users.noreply.github.com>
2022-05-30 17:56:07 +0300

    Delete Fotis DDI pipeline CODE directory

--- Fotis DDI pipeline CODE/orchestration-script.sh
+++
@@ -1,5 +0,0 @@
-sudo java -jar /home/fotis/workspace/db-umls-mapping.jar /home/fotis/workspace/DDI\ Prediction/MapDrugbankUMLS/ /home/fotis/workspace/DDI\ Prediction/ProcessNeo4j/ 
-sudo java -jar /home/fotis/workspace/paths-extraction.jar /home/fotis/workspace/DDI\ Prediction/ProcessNeo4j/PositivePairs/ /var/lib/neo4j/data/databases/graph.db
-sudo java -jar /home/fotis/workspace/paths-extraction.jar /home/fotis/workspace/DDI\ Prediction/ProcessNeo4j/NegativePairs/ /var/lib/neo4j/data/databases/graph.db
-java -jar /home/fotis/workspace/feature-extraction.jar
-python ./workspace/DDI\ Prediction/PythonClassifiers/random-forest-DDIs-approachB.py
